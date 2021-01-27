57dddc75f3fc553a617dbe1ae00c4790b41780f0
pedroszg <pedro.zuniman@gmail.com>
2021-01-27 07:52:43 -0600

    Update custom command, we delete unnecessary fields
    -time_given
    -time_asked

--- src/proposal/management/commands/set_up_project.py
+++ src/proposal/management/commands/set_up_project.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from proposal.models import Room, Speech, SpeechType, Topic, SpeechTime
+from proposal.models import Room, Speech, SpeechType, Topic
 from ecsl.models import EventECSL
 from django.contrib.auth.models import User
 
@@ -8,15 +8,13 @@ class Command(BaseCommand):
 
     def speech_creator(self):
 
-        time_asked = SpeechTime.objects.order_by("?").first()
-
         speeaches = [Speech(user=User.objects.first(), speaker_information="speaker {}".format(number),
                                          title="Speech {}".format(number), description="description {}".format(number),
                                          topic=Topic.objects.order_by("?").first(), audience="audience {}".format(number),
                                          skill_level=1, notes="notes {}".format(number),
-                                         speech_type=SpeechType.objects.order_by("?").first(), presentacion="presentacion {}".format(number),
-                                         event=EventECSL.objects.filter(current=True).first(), time_asked=time_asked,
-                                         time_given = time_asked, is_scheduled=False)
+                                         speech_type=SpeechType.objects.filter(is_special=False).
+                                         order_by("?").first(), presentacion="presentacion {}".format(number),
+                                         event=EventECSL.objects.filter(current=True).first())
                                          for number in range(6)]
 
         return speeaches
