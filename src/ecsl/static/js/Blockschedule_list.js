922daeea904797d9f5c3af1b75663d3627cf6b1b
mariovaargas <mariovargasc97@gmail.com>
2022-02-23 11:33:27 -0600

    Added FullCalendar config to support drag and drop events

--- src/ecsl/static/js/Blockschedule_list.js
+++ src/ecsl/static/js/Blockschedule_list.js
@@ -1,8 +1,27 @@
 document.addEventListener('DOMContentLoaded', function() {
+    var Calendar = FullCalendar.Calendar;
+    var Draggable = FullCalendar.Draggable;
+
+    var containerEl = document.getElementById('draggable-events');
     var calendarEl = document.getElementById('calendar');
-    var calendar = new FullCalendar.Calendar(calendarEl, {
+
+    // Initialize external events
+    new Draggable(containerEl, {
+        itemSelector: '.ui-state-default',
+        eventData: function(eventEl) {
+            return {
+                title: eventEl.innerText
+            };
+        }
+    });
+
+    // Initialize the calendar
+    var calendar = new Calendar(calendarEl, {
+        editable: true,
+        droppable: true,
         initialView: 'timeGridWeek',
         allDaySlot: false
     });
+
     calendar.render();
 });
