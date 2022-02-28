8670e53341ca774ce044bba71fa55fce28ae432d
ricardoalfaro <rialfaro98@gmail.com>
2022-02-28 17:27:22 -0600

    refactor to save objects in types.py and topics.py, add event id to index context, add new button to edit agenda only if staff,missing jquery functionality on modals

--- src/proposal/types.py
+++ src/proposal/types.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
@@ -13,17 +14,23 @@ from proposal.models import SpeechType
 class CreateType(generic.CreateView):
     model = SpeechType
     form_class = TypeForm
-    success_url = reverse_lazy('edit_charlas')
     # permission_required = 'proposal.add_speechtype'
 
     def form_valid(self, form):
+        event_id = self.request.POST.get('event_id')
         new_type = SpeechType.objects.create(
             name=form.cleaned_data['name'],
             time=form.data['eventTime'],
             event=EventECSL.objects.filter(current=True).first(),
+            # que pasa si tengo 2 current True, pueden haber varios current
+            # dentro del form mando el id del build agenda.
         )
-        new_type.save()
-        return redirect(reverse_lazy('edit_charlas'))
+        instance = {
+            "type_name": new_type.name,
+            "type_color": new_type.color,
+            "type_event": new_type.event,
+        }
+        return JsonResponse(instance)
 
     def form_invalid(self, form):
-        return redirect(reverse_lazy('edit_charlas'))
+        return JsonResponse("Ocurrio un error al guardar su tipo, intentelo de nuevo", status=400)
