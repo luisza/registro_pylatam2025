e40ee250c15359896c9ad8e4bdea0037b8baa9e1
Luis Zárate Montero <luis.zarate@solvosoft.com>
2024-06-06 20:16:52 -0600

    Create base template integration bootstrap 5

--- src/poge/urls.py
+++ src/poge/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from presentation.urls import urlpatterns as presentationurls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+] + presentationurls
